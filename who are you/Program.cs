d478a07e8f766ca1ce238e19c5fc751b76afe1c6
MrPedroGabriel123 <joaofelipechannel0@gmail.com>
2022-02-25 22:40:45 +0000

    kinda update

--- who are you/Program.cs
+++ who are you/Program.cs
@@ -1,2 +1,17 @@
 ﻿
-Console.WriteLine("Hello, World!");
+namespace GettingInput
+{
+    class whoareyou
+    {
+        static void Main()
+        {
+            Console.WriteLine("Whats your name?");
+            var nome = Console.ReadLine();
+            Console.WriteLine($"Hello, {nome}!");
+            Console.WriteLine("how old are you?");
+            var idade = Console.ReadLine();
+            Console.WriteLine($"So your name is {nome} and you are {idade} years old! ");
+            
+        }
+    }
+}
